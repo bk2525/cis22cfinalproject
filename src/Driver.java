587aa480bbf7d791d0bec5bbdf524a02e99f1c95
slkyker <174975582+slkyker@users.noreply.github.com>
2025-06-11 17:26:13 -0700

    Improve abstraction and clean up Driver class
    Moved stray program logic out of the Driver class and into the new
    CLInterface class to improve abstraction and error handling.
    Additionally, removed code related to debugging and cleaned up some
    style guide issues.
    
    * Removed IS_PROD boolean and related debugging messages
    * Removed Stephen L's testing code
    * Moved REMOVED_WORDS to class: ImportSongs
    * Moved removeWords() to class: ImportSongs
    * Adjusted line breaks to proceed + operators when wrapping a line
    
    These changes improve abstraction and ensure the Driver class is
    primarily tasked with the sole purpose of managing the runtime
    environment.

--- src/Driver.java
+++ src/Driver.java
@@ -7,11 +7,6 @@ import java.util.Scanner;
  */
 public class Driver {
     /**
-     * Whether the program should be compiled in production mode
-     */
-    private static final boolean IS_PROD = false;
-
-    /**
      * The minimum required JRE version supported by this program;
      * Java 11 or later is strongly recommended
      */
@@ -22,61 +17,19 @@ public class Driver {
      * @param args the entry point arguments, if any
      */
     public static void main(String[] args) {
-        // Check JRE version
+        // Validate JRE version
         try {
             checkVersion();
-            if (!Driver.IS_PROD) { // Log success if not in production
-                System.out.println("JRE valid. Launching program.");
-            }
-        } catch (NullPointerException |
-                 NumberFormatException |
-                 IllegalStateException e) {
+        } catch (NullPointerException
+                 | NumberFormatException
+                 | IllegalStateException e) {
             System.err.println(e.getMessage());
             System.exit(1);
         }
 
-        // Version validated and program can begin...
-        
-        //Stephen L's testing code, feel free to remove
-        Scanner input = new Scanner(System.in);
-        while (true) {
-        	System.out.println("Enter a string: ");
-        	System.out.println(removeWords(input.nextLine()));
-        }
-        
-        
-
+        // Version validated and program can begin
+        new CLInterface().init();
     }
-    
-    private final static ArrayList<String> REMOVED_WORDS = new ArrayList<>(Arrays.asList(
-    	    "a", "am", "and", "as", "at", "by", "but", "for", "i", "id", "if", "ill", "im",
-    	    "in", "is", "it", "its", "ive", "me", "my", "ooh", "of", "oh", "on", "or", "so",
-    	    "the", "that", "theyve", "to", "too", "us", "we", "we're", "ya", "yeah", "you",
-    	    "youll", "youre", "youve", "your", "was", "were", "they", "youd"
-    	));
-    
-    public static String removeWords(String input) {
-    	input = input.replaceAll("[^a-zA-Z0-9\\s]", "").toLowerCase();  //remove all punctuation, case insensitive
-        ArrayList<String> words = new ArrayList<>(Arrays.asList(input.split("\\s+"))); //split the input into individual pieces into an arraylist
-
-        ArrayList<String> filteredWords = new ArrayList<>(); //arraylist to hold the valid words
-        
-        for (String word : words) {
-            boolean shouldRemove = false;
-            for (String removed : REMOVED_WORDS) {
-                if (word.equals(removed)) {
-                    shouldRemove = true;
-                    break;
-                }
-            }
-            if (!shouldRemove) {
-                filteredWords.add(word);
-            }
-        }
-        return String.join(" ", filteredWords);
-    }
-    
-    
 
     /**
      * Checks whether the user's Java Runtime Environment is a version
@@ -100,9 +53,9 @@ public class Driver {
         // Null check
         if (reqVersion == null || javaVersion == null) {
             throw new NullPointerException(String.format(
-                "checkVersion(): Strings must be non-null.%n" +
-                "  reqVersion:  %s%n" +
-                "  javaVersion: %s%n",
+                "checkVersion(): Strings must be non-null.%n"
+                + "  reqVersion:  %s%n"
+                + "  javaVersion: %s%n",
                 reqVersion, javaVersion));
         }
 
@@ -134,17 +87,17 @@ public class Driver {
             if (Integer.parseInt(runtimeVersion) <
                 Integer.parseInt(reqVersion)) {
                 throw new IllegalStateException(String.format(
-                    "checkVersion(): Unsupported Java version.%n" +
-                    "  Minimum supported runtime version: %s%n" +
-                    "  Current runtime version:           %s%n",
+                    "checkVersion(): Unsupported Java version.%n"
+                    + "  Minimum supported runtime version: %s%n"
+                    + "  Current runtime version:           %s%n",
                     reqVersion, runtimeVersion));
             }
         } catch (NumberFormatException nfe) {
             throw new NumberFormatException(String.format(
-                "checkVersion(): Version parse and compare failed " +
-                "due to an invalid format.%n" +
-                "  reqVersion:     %s%n" +
-                "  runtimeVersion: %s%n",
+                "checkVersion(): Version parse and compare failed "
+                + "due to an invalid format.%n"
+                + "  reqVersion:     %s%n"
+                + "  runtimeVersion: %s%n",
                 reqVersion, runtimeVersion));
         }
     }
